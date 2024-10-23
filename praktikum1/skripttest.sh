7ea768ca75d34bf506c8af14862bedd749048902
Mati Lillemaa <mati.lillemaa@voco.ee>
2024-10-23 12:48:22 +0300

    see on esimene skript kolme käsuga

---
+++ praktikum1/skripttest.sh
@@ -0,0 +1,9 @@
+### see on testimiseks esimene skript
+echo -n "oled kasutaja nimega: " 
+whoami 
+sleep 1
+echo "ja sina asud kataloogis: " 
+pwd 
+sleep 1
+echo "selle kataloogi sisu(list) on: " 
+ls -l
